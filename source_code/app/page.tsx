1c1fc2f91da026b75690799d7d6883d8974ebea0
Kevin Gomez <kevin.gomez6@upr.edu>
2025-09-28 22:02:44 -0400

    Added SellPiece page wireframe as per issue [Feature]: Implement Piece Selling Page Wireframe/Layout Fixes uprm-inso4116-2025-2026-s1/semester-project-uprm-hand_me_down_clothing#137

--- source_code/app/page.tsx
+++ source_code/app/page.tsx
@@ -1,7 +1,16 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <div>
-      Hello world!
-    </div>
+    <main className='p-6'>
+      <div>
+        Hello world!
+      </div>
+      <div className="text-white">
+        <Link href="/listings/sell_piece" className="inline-block rounded-md bg-indigo-500 px-4 py-2 text-white hover:bg-indigo-400">
+          Go to Sell Page
+        </Link>
+      </div>
+    </main>
   );
 }
