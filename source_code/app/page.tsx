0c0b8bd990ec8caae6c4ae1992e3b08f2b54051f
leanelys <leanelys.gonzalez@upr.edu>
2025-09-30 22:25:43 -0400

    Add button to main page for donating item

--- source_code/app/page.tsx
+++ source_code/app/page.tsx
@@ -1,7 +1,16 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <div>
-      Hello world!
-    </div>
+    <main className='p-6'>
+      <div>
+        Hello world!
+      </div>
+      <div className="text-black">
+        <Link href="/listings/donate_piece" className="inline-block border rounded-lg p-2">
+          Donate Page
+        </Link>
+      </div>
+    </main>
   );
 }
