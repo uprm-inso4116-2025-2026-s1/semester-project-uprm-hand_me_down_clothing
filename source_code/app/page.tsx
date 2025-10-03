a4fca0daa67d6642329a60b4c5dbdfdbacd51c2c
Alma-pineiro <alma.pineiro1@upr.edu>
2025-10-03 13:26:32 -0400

    eliminated buttons in home

--- source_code/app/page.tsx
+++ source_code/app/page.tsx
@@ -6,14 +6,7 @@ export default function Home() {
       <div>
         Hello world!
       </div>
-      <div className="text-black">
-        <Link href="/listings/donate_piece" className="inline-block border rounded-lg p-2">
-          Donate Page
-      <div className="text-black dark:text-white">
-        <Link href="/listings/sell_piece" className="inline-block rounded-md bg-indigo-500 px-4 py-2 text-white hover:bg-indigo-400">
-          Go to Sell Page
-        </Link>
-      </div>
+    
     </main>
   );
 }
